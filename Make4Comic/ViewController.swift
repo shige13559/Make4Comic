618c2920efe5613beb62178a138d08998d5a9c55
原田茂大 <sapori946@gmail.com>
2020-01-14 20:07:54 +0800

    no message

---
+++ Make4Comic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Make4Comic
+//
+//  Created by 原田茂大 on 2020/01/14.
+//  Copyright © 2020 geshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
